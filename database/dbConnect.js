eaf55b99ba384cc5221d4e85f28478d97e9bbbdb
manendra29 <1rn21is084.manendrakushwaha@gmail.com>
2025-03-26 23:15:01 +0530

    Update 7

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 
-export const dbConnect= ()=>{
-    mongoose.connect(process.env.DATABASE_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("🔥 MongoDB Connected Successfully!"))
-    .catch((err) => console.error("❌ MongoDB Connection Error:", err));
-}
+export const dbConnect = () => {
+    mongoose.connect(process.env.DATABASE_URI)
+        .then(() => console.log("🔥 MongoDB Connected Successfully!"))
+        .catch((err) => console.error("❌ MongoDB Connection Error:", err));
+};
