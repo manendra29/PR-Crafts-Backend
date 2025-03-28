26ba2df9c7770ee3905a708ea7af64f9ae5cf2c3
manendra29 <1rn21is084.manendrakushwaha@gmail.com>
2025-03-28 09:27:12 +0530

    Update 9

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,7 +1,7 @@
 export const generateJwtToken=(user,message,statusCode,res)=>{
     const token=user.generateJsonWebToken();
     res.status(statusCode).cookie("token",token,{
-        expires:new Date(Date.now()+process.env.COOKIE_EXPIRES*24*60*60*1000),
+        expires:new Date(Date.now()+7*24*60*60*1000),
         httpOnly:true
     }).json({
         success:true,
