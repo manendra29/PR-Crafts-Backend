2d6f87e198decf85b76bb5471e8ffab34785f030
manendra29 <1rn21is084.manendrakushwaha@gmail.com>
2025-03-28 10:11:01 +0530

    Update 11

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,12 +1,13 @@
-export const generateJwtToken=(user,message,statusCode,res)=>{
-    const token=user.generateJsonWebToken();
-    res.status(statusCode).cookie("token",token,{
-        expires:new Date(Date.now()+7*24*60*60*1000),
-        httpOnly:true
-    }).json({
-        success:true,
-        message,
-        user,
-        token
-    });     
-}
+res.status(statusCode)
+  .cookie("token", token, {
+    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // Secure only in production
+    sameSite: "None", // Needed for cross-origin requests
+  })
+  .json({
+    success: true,
+    message,
+    user,
+    token,
+  });
