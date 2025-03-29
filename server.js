ace2dfba4bfb041646ac3d331912d3c3ad24cf84
manendra29 <1rn21is084.manendrakushwaha@gmail.com>
2025-03-29 10:44:05 +0530

    Update 24

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ import dotenv from "dotenv";
 // Load .env variables
 dotenv.config();  
 
+console.log("Cloudinary Config:");
+console.log("CLOUDINARY_CLOUD_NAME:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("CLOUDINARY_API_KEY:", process.env.CLOUDINARY_API_KEY);
+console.log("CLOUDINARY_API_SECRET:", process.env.CLOUDINARY_API_SECRET);
+
 // Cloudinary configuration
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,  // Ensure the key name is correct
+    cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_SECRET_KEY
 });
+
 
 app.listen(process.env.PORT || 4000, () => {
     console.log(`Server is running on port ${process.env.PORT || 4000}`);
